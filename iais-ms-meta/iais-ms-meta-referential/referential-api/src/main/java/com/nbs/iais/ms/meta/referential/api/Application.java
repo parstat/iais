4b4dae26e7ad8e8579ddcdb4697a5464b9714c3f
florianika <florianika@gmail.com>
2020-03-21 14:48:10 +0100

    continue working on the iais structure

---
+++ iais-ms-meta/iais-ms-meta-referential/referential-api/src/main/java/com/nbs/iais/ms/meta/referential/api/Application.java
@@ -0,0 +1,13 @@
+package com.nbs.iais.ms.meta.referential.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
+
