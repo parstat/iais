a5c2e20933a96487148846d7090159e0473becb6
florianika <florianika@gmail.com>
2020-03-21 16:31:18 +0100

    continue working on the iais structure

---
+++ iais-ms-processing/processing-db/src/main/java/com/nbs/iais/ms/processing/db/Application.java
@@ -0,0 +1,13 @@
+package com.nbs.iais.ms.processing.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
