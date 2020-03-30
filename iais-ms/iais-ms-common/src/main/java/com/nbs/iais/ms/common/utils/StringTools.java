ca0a90516f5a2c96bedcb05725b5ee7a00899e2d
florianika <florianika@gmail.com>
2020-03-30 10:55:45 +0200

    replaced log4j with spring boot version

--- iais-ms/iais-ms-common/src/main/java/com/nbs/iais/ms/common/utils/StringTools.java
+++ iais-ms/iais-ms-common/src/main/java/com/nbs/iais/ms/common/utils/StringTools.java
@@ -1,8 +1,8 @@
 package com.nbs.iais.ms.common.utils;
 
 import com.nbs.iais.ms.common.exceptions.SystemSetupException;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
