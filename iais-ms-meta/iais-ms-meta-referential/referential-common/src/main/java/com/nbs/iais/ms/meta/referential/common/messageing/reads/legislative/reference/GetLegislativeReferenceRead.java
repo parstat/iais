fc716216fbc47c876dbab03d9f49f0ab91fbfaf0
Francesco Amato <francamato@gmail.com>
2020-05-04 01:14:17 +0200

    adding Legislative Reference

---
+++ iais-ms-meta/iais-ms-meta-referential/referential-common/src/main/java/com/nbs/iais/ms/meta/referential/common/messageing/reads/legislative/reference/GetLegislativeReferenceRead.java
@@ -0,0 +1,14 @@
+package com.nbs.iais.ms.meta.referential.common.messageing.reads.legislative.reference;
+
+import com.nbs.iais.ms.common.dto.impl.LegislativeReferenceDTO;
+import com.nbs.iais.ms.common.messaging.reads.abstracts.AbstractRead;
+
+public class GetLegislativeReferenceRead extends AbstractRead<LegislativeReferenceDTO> {
+
+    private static final long serialVersionUID = 2004L;
+
+    public GetLegislativeReferenceRead() {
+        super();
+    }
+
+}
