6130ebade18684478d9385bab96684c287c9c6f4
florianika <florianika@gmail.com>
2020-05-06 11:56:07 +0200

    fixed cors problem in zuul

--- iais-cloud/iais-zuul/src/main/java/com/nbs/iais/cloud/zuul/ZuulApplication.java
+++ iais-cloud/iais-zuul/src/main/java/com/nbs/iais/cloud/zuul/ZuulApplication.java
@@ -2,13 +2,17 @@ package com.nbs.iais.cloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 import java.util.Collections;
 
 @SpringBootApplication(exclude = {
@@ -23,25 +27,28 @@ public class ZuulApplication {
     }
 
 
-    @Order(0)
     @Bean
-    public CorsFilter corsFilter() {
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
+
         config.setAllowCredentials(true);
         config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
+        config.addAllowedOrigin("http://localhost:8090");
+        config.setAllowedMethods(Arrays.asList(RequestMethod.GET.name(),
+                RequestMethod.POST.name(),
+                RequestMethod.OPTIONS.name(),
+                RequestMethod.DELETE.name(),
+                RequestMethod.PUT.name(),
+                RequestMethod.PATCH.name()));
         config.setExposedHeaders(Collections.singletonList("jwt-auth"));
         config.setAllowedHeaders(Collections.singletonList("jwt-auth"));
+        config.addAllowedHeader("*");
         source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
 
-        return new CorsFilter(source);
     }
 }
