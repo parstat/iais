f7aa78c1e85f9db44a563f61b5cac56c5a6d2ec7
NataliaGheorghita <natalia.gheorghita@statistica.gov.md>
2020-06-02 19:19:34 +0300

    Add test for `save` method from repository `ProcessMethodRepository`

---
+++ iais-ms-meta/iais-ms-meta-referential/referential-db/src/test/java/com/nbs/iais/ms/meta/referential/db/repository/ProcessMethodRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.nbs.iais.ms.meta.referential.db.repository;
+
+import com.nbs.iais.ms.common.db.repository.tests.RepositoryTest;
+import com.nbs.iais.ms.common.enums.Language;
+import com.nbs.iais.ms.meta.referential.db.domains.gsim.ProcessMethodEntity;
+import com.nbs.iais.ms.meta.referential.db.repositories.ProcessMethodRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class ProcessMethodRepositoryTest extends RepositoryTest {
+
+    @Autowired
+    private ProcessMethodRepository processMethodRepository;
+
+    @Test
+    public void testSave() {
+        final ProcessMethodEntity toSaveEntity = createEntity();
+        final ProcessMethodEntity savedEntity = processMethodRepository.save(toSaveEntity);
+
+        Assert.assertNotNull(savedEntity);
+    }
+
+    private ProcessMethodEntity createEntity() {
+        final ProcessMethodEntity entity = new ProcessMethodEntity();
+
+        // set required fields
+        entity.setLocalId("lfs");
+        entity.setVersion("1.0");
+        entity.setVersionDate(LocalDateTime.now());
+
+        // set optional fields
+        entity.setName("Name", Language.ENG);
+        entity.setName("Nume", Language.ROM);
+        entity.setName("имя", Language.RUS);
+        entity.setDescription("Description", Language.ENG);
+        entity.setDescription("Descriere", Language.ROM);
+        entity.setDescription("Oписание", Language.RUS);
+
+        return entity;
+    }
+
+
+}
