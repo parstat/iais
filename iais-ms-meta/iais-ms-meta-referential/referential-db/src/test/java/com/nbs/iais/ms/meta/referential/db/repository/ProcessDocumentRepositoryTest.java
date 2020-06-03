61d072df932cc4511f282380749423643e23e5d6
iostream17 <17309940+iostream17@users.noreply.github.com>
2020-06-03 12:01:58 +0300

    Add test for `save` method from repository `ProcessDocumentRepository`

---
+++ iais-ms-meta/iais-ms-meta-referential/referential-db/src/test/java/com/nbs/iais/ms/meta/referential/db/repository/ProcessDocumentRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.nbs.iais.ms.meta.referential.db.repository;
+
+import com.nbs.iais.ms.common.db.repository.tests.RepositoryTest;
+import com.nbs.iais.ms.common.enums.Language;
+import com.nbs.iais.ms.common.enums.AgentType;
+import com.nbs.iais.ms.meta.referential.db.domains.gsim.AgentEntity;
+import com.nbs.iais.ms.meta.referential.db.repositories.AgentRepository;
+import com.nbs.iais.ms.meta.referential.db.domains.gsim.ProcessDocumentEntity;
+import com.nbs.iais.ms.meta.referential.db.repositories.ProcessDocumentRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class ProcessDocumentRepositoryTest extends RepositoryTest {
+
+    @Autowired
+    ProcessDocumentRepository processDocumentRepository;
+
+    @Test
+    public void testSaveBusinessFunction () {
+    final  ProcessDocumentEntity toSaveDoc = saveProcessDocumentRepository();
+    final  ProcessDocumentEntity saveDoc = processDocumentRepository.save(toSaveDoc);
+    Assert.assertNotNull(saveDoc);
+
+    }
+
+    private ProcessDocumentEntity saveProcessDocumentRepository(){
+
+            final ProcessDocumentEntity toSave = new ProcessDocumentEntity();
+
+           // set required fields
+            toSave.setLocalId("lsf");
+            toSave.setVersion("1.0");
+            toSave.setVersionDate(LocalDateTime.now());
+        // set optional fields
+
+        toSave.setName("Name",Language.ENG);
+        toSave.setName("Nume",Language.ROM);
+        toSave.setName("имя",Language.RUS);
+        toSave.setDescription("Description",Language.ENG);
+        toSave.setDescription("Descriere",Language.ROM);
+        toSave.setDescription("Oписание",Language.RUS);
+
+
+            return toSave;
+
+    }
+
+}
