0c1e764863d1dab7fc0b8ca27fa1cfc09048c96a
florianika <florianika@gmail.com>
2020-06-08 17:11:38 +0200

    adding AuthenticateFilter in zuul

--- iais-cloud/iais-zuul/src/main/java/com/nbs/iais/cloud/zuul/config/FilterConfig.java
+++ iais-cloud/iais-zuul/src/main/java/com/nbs/iais/cloud/zuul/config/FilterConfig.java
@@ -2,10 +2,7 @@ package com.nbs.iais.cloud.zuul.config;
 
 import com.nbs.iais.cloud.zuul.filters.post.SigninJwtFilter;
 import com.nbs.iais.cloud.zuul.filters.post.SignupJwtFilter;
-import com.nbs.iais.cloud.zuul.filters.pre.AuthenticatedFilter;
-import com.nbs.iais.cloud.zuul.filters.pre.LanguageFilter;
-import com.nbs.iais.cloud.zuul.filters.pre.SignoutFilter;
-import com.nbs.iais.cloud.zuul.filters.pre.XssFilter;
+import com.nbs.iais.cloud.zuul.filters.pre.*;
 import com.nbs.iais.ms.common.enums.Language;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,6 +34,11 @@ public class FilterConfig {
     @Bean
     public SignoutFilter signoutFilter() {
         return new SignoutFilter();
+    }
+
+    @Bean
+    public AuthenticateFilter authenticateFilter() {
+        return new AuthenticateFilter();
     }
 
     @Bean
---
+++ iais-cloud/iais-zuul/src/main/java/com/nbs/iais/cloud/zuul/filters/pre/AuthenticateFilter.java
@@ -0,0 +1,80 @@
+package com.nbs.iais.cloud.zuul.filters.pre;
+
+import com.auth0.jwt.exceptions.*;
+import com.nbs.iais.cloud.zuul.jwt.service.SecurityService;
+import com.nbs.iais.ms.common.utils.StringTools;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+
+public class AuthenticateFilter extends ZuulFilter {
+
+    @Value("${iais.jwt.header.name}")
+    private final String jwtHeaderName = "jwt-auth";
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        final RequestContext ctx = RequestContext.getCurrentContext();
+        final HttpServletRequest request = ctx.getRequest();
+        final String requestUri = request.getRequestURI();
+        final String method = request.getMethod();
+        // this filter should run for every request except security and closed requests
+        return !method.equals(HttpMethod.OPTIONS.toString())
+                && requestUri.matches(".*/api/v[12]/{0,1}\\w*/{0,1}\\w*/{0,1}\\w*/(authenticate).*");
+    }
+
+    @Override
+    public Object run() {
+        final RequestContext ctx = RequestContext.getCurrentContext();
+        final HttpServletRequest request = ctx.getRequest();
+        final String jwtAuthHeader = request.getHeader(jwtHeaderName);
+
+        if(StringTools.isEmpty(jwtAuthHeader)) {
+            //the user is not signed in
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(HttpStatus.BAD_REQUEST.value());
+            return null;
+        }
+
+        try {
+            securityService.verifyJwt(jwtAuthHeader);
+            ctx.setSendZuulResponse(false);
+            ctx.addZuulResponseHeader(jwtHeaderName, jwtAuthHeader);
+            ctx.setResponseStatusCode(HttpStatus.OK.value());
+            return null;
+        } catch (TokenExpiredException ex) {
+            final String jwt = securityService.refreshToken(jwtAuthHeader);
+            ctx.addZuulResponseHeader(jwtHeaderName, jwt);
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(HttpStatus.CREATED.value());
+            return null;
+        } catch (AlgorithmMismatchException | InvalidClaimException |
+                JWTDecodeException | SignatureVerificationException ex) {
+            //Shouldn't go here Zuul will mark as bad request if jwt is empty or jwt is not provided by iais
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+            return null;
+        }
+
+    }
+}
